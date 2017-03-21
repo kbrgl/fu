42275b421363d450149462d715558da80c530211
Kabir Goel <kabirgoel.kg@gmail.com>
2017-03-21 11:57:57 +0530

    Added archive version feature to create_release_archives.sh

--- create_release_archives.sh
+++ create_release_archives.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-if [[ -d "build" && -x "build" ]]; then
-    rm -r build
+
+read -p "Release Version> " version
+
+re="^v?[0-9]+\.[0-9]+\.[0-9]+$"
+
+# Ensure that the version conforms to version naming requirements
+if ! [[ $version =~ $re ]]; then
+  echo "Error: version number must fit semver naming requirements"
+  exit 1
 fi
+
+echo "Creating release archives for Fu $version..."
+
+if [[ -d "build" && -x "build" ]]; then
+  rm -rf build
+fi
+
 echo "Compiling..."
 if hash gox 2>/dev/null; then
-    gox -output "build/fu_{{.OS}}_{{.Arch}}/fu" -os "linux darwin" -arch "amd64" -ldflags "-s -w"
+  gox -output "build/fu-$version-{{.OS}}-{{.Arch}}/fu" -os "linux darwin" -arch "amd64" -ldflags "-s -w"
 else
-    echo "gox is not installed, can install it by running `go get github.com/mitchellh/gox`."
-    exit 1
+  echo "Error: gox is not installed, can install it by running `go get github.com/mitchellh/gox`"
+  exit 1
 fi
+
 cd build
+
 echo -n "Compressing... "
-tar czf fu_darwin_amd64.tar.gz fu_darwin_amd64/
-tar czf fu_linux_amd64.tar.gz fu_linux_amd64/
+tar czf "fu-$version-darwin-amd64.tar.gz" "fu-$version-darwin-amd64/"
+tar czf "fu-$version-linux-amd64.tar.gz" "fu-$version-linux-amd64/"
 echo "done."
+
 echo -n "Cleaning up... "
-rm -rf fu_darwin_amd64
-rm -rf fu_linux_amd64
+rm -rf "fu-$version-darwin-amd64"
+rm -rf "fu-$version-linux-amd64"
 echo "done."
+
 echo "All done."
