4b805990a7aeb74631dfa9a62743556b665de83b
Kabir Goel <kabirgoel.kg@gmail.com>
2017-09-26 21:38:04 +0530

    v2.2.0 - parallel by default and code improvements
    Deprecated parallel flag (--parallel / -c) in favour of explicit sequential search flag (--seq)
    
    Main function has been broken up into several smaller functions

--- fu.go
+++ fu.go
@@ -9,110 +9,64 @@ import (
 	"time"
 
 	"github.com/alecthomas/kingpin"
+	"github.com/fatih/color"
 	"github.com/kbrgl/fu/matchers"
 	"github.com/kbrgl/fu/shallowradix"
+	isatty "github.com/mattn/go-isatty"
 	"github.com/stretchr/powerwalk"
 )
 
 const (
 	// Version is the program version
-	Version = "2.1.1"
+	Version = "2.2.0"
 )
 
-func main() {
-	kingpin.CommandLine.HelpFlag.Short('h')
-	fz := kingpin.Flag("fuzzy", "Use fuzzy search").Short('f').Bool()
-	re := kingpin.Flag("regexp", "Use regexp-based search").Short('r').Bool()
-	sfx := kingpin.Flag("suffix", "Use suffix-based search (short flag 'a' is short for 'after')").Short('a').Bool()
-	pfx := kingpin.Flag("prefix", "Use prefix-based search (short flag 'b' is short for 'before')").Short('b').Bool()
-	substring := kingpin.Flag("substring",
+var (
+	fz        = kingpin.Flag("fuzzy", "Use fuzzy search").Short('f').Bool()
+	re        = kingpin.Flag("regexp", "Use regexp-based search").Short('r').Bool()
+	sfx       = kingpin.Flag("suffix", "Use suffix-based search (short flag 'a' is short for 'after')").Short('a').Bool()
+	pfx       = kingpin.Flag("prefix", "Use prefix-based search (short flag 'b' is short for 'before')").Short('b').Bool()
+	substring = kingpin.Flag("substring",
 		"Use substring-based search allowing the query to be at any position in the filename").
 		Short('s').Bool()
-	dir := kingpin.Flag("dir", "Show only directories").Short('d').Bool()
-	perm := kingpin.Flag("perm", "Filter by Unix permissions").Short('m').Int()
-	pll := kingpin.Flag("parallel",
-		"Walk directories in parallel, may result in substantial speedups "+
+	dir     = kingpin.Flag("dirs", "Show only directories").Short('d').Bool()
+	perm    = kingpin.Flag("perm", "Filter by Unix permissions").Short('m').Int()
+	seq     = kingpin.Flag("seq", "Search directories sequentially").Bool()
+	older   = kingpin.Flag("older", "Filter by age (modification time)").Short('o').Duration()
+	younger = kingpin.Flag("younger", "Filter by age (modification time)").Short('y').Duration()
+	exclude = kingpin.Flag("exclude", "Excludes files matching the filters").Short('e').Bool()
+	_       = kingpin.Flag("parallel",
+		"[DEPRECATED: see --seq] Walk directories in parallel, may result in substantial speedups "+
 			"for directories with many files").
 		Short('c').Bool()
-	older := kingpin.Flag("older", "Filter by age (modification time)").Short('o').Duration()
-	younger := kingpin.Flag("younger", "Filter by age (modification time)").Short('y').Duration()
-	exclude := kingpin.Flag("exclude", "Excludes files matching the filters").Short('e').Bool()
 
-	query := kingpin.Arg("query", "Search query").Required().String()
-	paths := kingpin.Arg("paths", "Paths to search").Default(".").ExistingDirs()
+	query = kingpin.Arg("query", "Search query").Required().String()
+	paths = kingpin.Arg("paths", "Paths to search").Default(".").ExistingDirs()
+)
+
+func init() {
+	kingpin.CommandLine.HelpFlag.Short('h')
 	kingpin.Version(Version)
 	kingpin.CommandLine.VersionFlag.Short('v')
 	kingpin.Parse()
+}
 
-	walk := filepath.Walk
-	if *pll {
-		runtime.GOMAXPROCS(runtime.NumCPU())
-		walk = powerwalk.Walk
-	}
+func main() {
+	walk := getWalker()
+	ms := getMatchers()
 
-	// Filename matchers can't be stacked (there can only be one), so check the
-	// flags and choose one.
-	// There's currently no way to fail if multiple filename matchers are provided,
-	// so the ordering of cases here is pretty much arbitrary.
-	var (
-		// name matcher
-		nm  matchers.FileMatcher
-		err error
-	)
-	switch {
-	case *re:
-		nm, err = matchers.NewREMatcher(*query)
-		if err != nil {
-			fail(err)
-		}
-	case *sfx:
-		nm = matchers.NewSuffixMatcher(*query)
-	case *pfx:
-		nm = matchers.NewPrefixMatcher(*query)
-	case *fz:
-		nm = matchers.NewFuzzyMatcher(*query)
-	case *substring:
-		nm = matchers.NewSubstringMatcher(*query)
-	default:
-		nm = matchers.NewExactMatcher(*query)
-	}
-
-	ms := make([]matchers.FileMatcher, 0, 3)
-	ms = append(ms, nm)
-
-	// Append stackable matchers to the matchers slice
-	if *perm != 0 {
-		ms = append(ms, matchers.NewPermMatcher(os.FileMode(*perm)))
-	}
-	if *dir {
-		ms = append(ms, matchers.NewDirMatcher())
-	}
-	if *older != 0 {
-		ms = append(ms, matchers.NewAgeOlderMatcher(*older))
-	}
-	if *younger != 0 {
-		ms = append(ms, matchers.NewAgeYoungerMatcher(*younger))
-	}
-
-	rdx := shallowradix.New()
-
-	for _, path := range *paths {
-		abs, err := filepath.Abs(path)
-		if err != nil {
-			fail(err)
-		}
-		rdx.Insert(fmt.Sprintf("%s%c", abs, os.PathSeparator))
-	}
-
-	// number of files found and number of files traversed
+	// number of files found / traversed
 	var found, traversed uint64
 	start := time.Now()
-	for _, path := range rdx.Prefixes() {
-		walk(path, func(path string, fi os.FileInfo, err error) error {
+	pathPrefixes := getPathPrefixes(*paths)
+	for _, path := range pathPrefixes {
+		_ = walk(path, func(path string, fi os.FileInfo, err error) error {
 			atomic.AddUint64(&traversed, 1)
 
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
+				if isatty.IsTerminal(os.Stderr.Fd()) {
+					errorf(err)
+				}
 				return nil
 			}
 
@@ -142,7 +96,96 @@ func main() {
 		found)
 }
 
+func getWalker() func(string, filepath.WalkFunc) error {
+	walk := filepath.Walk
+	if !*seq {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		walk = powerwalk.Walk
+	}
+	return walk
+}
+
+func getMatchers() []matchers.FileMatcher {
+	// Filename matchers can't be stacked (there can only be one), so check the
+	// flags and choose one.
+	// There's currently no way to fail if multiple filename matchers are provided,
+	// so the ordering of cases here is pretty much arbitrary.
+	nm := getNameMatcher()
+
+	ms := make([]matchers.FileMatcher, 0, 3)
+	ms = append(ms, nm)
+
+	// Append stackable matchers to the matchers slice
+	if *perm != 0 {
+		ms = append(ms, matchers.NewPermMatcher(os.FileMode(*perm)))
+	}
+	if *dir {
+		ms = append(ms, matchers.NewDirMatcher())
+	}
+	if *older != 0 {
+		ms = append(ms, matchers.NewAgeOlderMatcher(*older))
+	}
+	if *younger != 0 {
+		ms = append(ms, matchers.NewAgeYoungerMatcher(*younger))
+	}
+
+	return ms
+}
+
+func getNameMatcher() matchers.FileMatcher {
+	var (
+		// name matcher
+		nm  matchers.FileMatcher
+		err error
+	)
+	switch {
+	case *re:
+		nm, err = matchers.NewREMatcher(*query)
+		if err != nil {
+			fail(err)
+		}
+	case *sfx:
+		nm = matchers.NewSuffixMatcher(*query)
+	case *pfx:
+		nm = matchers.NewPrefixMatcher(*query)
+	case *fz:
+		nm = matchers.NewFuzzyMatcher(*query)
+	case *substring:
+		nm = matchers.NewSubstringMatcher(*query)
+	default:
+		if len(*query) == 0 {
+			nm = matchers.NewAllMatcher()
+		} else {
+			nm = matchers.NewExactMatcher(*query)
+		}
+	}
+
+	return nm
+}
+
+func getPathPrefixes(paths []string) []string {
+	rdx := shallowradix.New()
+
+	for _, path := range paths {
+		abs, err := filepath.Abs(path)
+		if err != nil {
+			fail(err)
+		}
+		rdx.Insert(fmt.Sprintf("%s%c", abs, os.PathSeparator))
+	}
+
+	return rdx.Prefixes()
+}
+
 func fail(err error) {
 	fmt.Fprintf(os.Stderr, "%v\n", err)
 	os.Exit(1)
+}
+
+func errorf(err error) {
+	if isatty.IsTerminal(os.Stderr.Fd()) {
+		color.Set(color.FgRed)
+		defer color.Unset()
+	}
+	fmt.Fprintf(os.Stderr, "%v\n", err)
 }
